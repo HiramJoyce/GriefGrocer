852da9b8317305bd4dd9a6dbe5c5977eedfd2338
曹海亮 <1050588656@qq.com>
2018-11-28 10:04:40 +0800

    Initial commit

---
+++ src/main/java/com/griefgrocer/GriefgrocerApplication.java
@@ -0,0 +1,12 @@
+package com.griefgrocer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GriefgrocerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GriefgrocerApplication.class, args);
+    }
+}
